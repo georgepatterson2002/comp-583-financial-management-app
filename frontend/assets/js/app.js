12b75ac3ca715e0f1639246390e6d3675f932019
ROHANLINGA <55657954+ROHANLINGA@users.noreply.github.com>
2025-02-26 17:14:02 -0800

    Update app.js

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,1 +1,14 @@
-//app.js
+<script>
+    // JavaScript to toggle between Sign Up and Log In forms
+    document.getElementById('loginLink').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default link behavior
+        document.getElementById('signup-container').style.display = 'none'; // Hide Sign Up form
+        document.getElementById('loginContainer').style.display = 'block'; // Show Log In form
+    });
+
+    document.getElementById('signupLink').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default link behavior
+        document.getElementById('loginContainer').style.display = 'none'; // Hide Log In form
+        document.getElementById('signup-container').style.display = 'block'; // Show Sign Up form
+    });
+</script>//app.js
